a3853cf87bc250ffaa9571f70ef4148b2c797ab1
zafarxayrullayev <xayrullayevzafar49@gamil.com>
2022-11-19 13:50:04 +0500

    matritsa tashkil qildim, uni elementlarini konsolga chiqardim

---
+++ matritsa_burish/matritsa_burish/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matritsa_burish
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n  = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
